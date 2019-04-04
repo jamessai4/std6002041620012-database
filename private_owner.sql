3937dfcbfe869b419d10d39bf31b82bf61bd6278
jamessai4 <48791720+jamessai4@users.noreply.github.com>
2019-04-04 17:37:50 +0700

    Add files via upload

--- private_owner.sql
+++ private_owner.sql
@@ -1,6 +1,5 @@
-drop table private_owner;
-create table private_owner(
-     id int primary key auto_increment,
+
+create table private_owner(id int primary key auto_increment,
      owner_no varchar(4) not null,
      first_name varchar(300) not null,
      last_name varchar(300) not null,
